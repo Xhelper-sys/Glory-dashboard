021d6df3ba525e189cdb1a835deaa4d469bc63ee
Xhelper-sys <mus.kya21@gmail.com>
2026-06-13 12:22:55 +0000

    task management improvement

--- scripts/todo.js
+++ scripts/todo.js
@@ -9,3 +9,43 @@ function addTask(){
   inputElement.value = '';
 
 }
+
+function priorityChangement(){
+  const buttonElement = document.querySelector('.js-priority-button');
+  const tabDecoCircle = [];
+
+  for (let i = 1; i < 7; i++){
+    tabDecoCircle[i - 1] = document.querySelector(`.js-around-button-${i}`);
+  }
+
+  if (buttonElement.innerText === 'Low') {
+    buttonElement.innerText = 'Medium';
+    buttonElement.classList.remove('green-low-priority-button');
+    buttonElement.classList.add('orange-medium-priority-button');
+    
+    for (let i = 1; i<7; i++){
+      tabDecoCircle[i - 1].classList.remove('color-circle-green');
+      tabDecoCircle[i - 1].classList.add('color-circle-orange');
+    }
+  } else if (buttonElement.innerText === 'Medium') {
+    buttonElement.innerText = 'High';
+    buttonElement.classList.remove('orange-medium-priority-button');
+    buttonElement.classList.add('red-hight-priority-button');
+
+    for (let i = 1; i<7; i++){
+      tabDecoCircle[i - 1].classList.remove('color-circle-orange');
+      tabDecoCircle[i - 1].classList.add('color-circle-red');
+    }
+  } else {
+    buttonElement.innerText = 'Low';
+    buttonElement.classList.remove('red-hight-priority-button');
+    buttonElement.classList.add('green-low-priority-button');
+
+    for (let i = 1; i<7; i++){
+      tabDecoCircle[i - 1].classList.remove('color-circle-red');
+      tabDecoCircle[i - 1].classList.add('color-circle-green');
+    }
+  }
+
+  console.log(tabDecoCircle);
+}
